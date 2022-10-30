c6a428124c0c124ff94930e486c5744b5f813122
jvcataquiz <jvcataquiz@gmail.com>
2022-10-30 16:15:13 +0800

    Create Employee Class inside the Model Package

---
+++ EmployeeManagement/src/main/java/com/example/EmployeeManagement/Model/Employee.java
@@ -0,0 +1,70 @@
+package com.example.EmployeeManagement.Model;
+
+public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String address;
+
+    public Employee() {
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public Employee(String firstName, String lastName, String emailAddress, String address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}

5366df999c95e93727898ca1acb31999e2a25f1c
jvcataquiz <jvcataquiz@gmail.com>
2022-10-30 16:35:33 +0800

    Create EmployeeController Class inside the Controller Package

---
+++ EmployeeManagement/src/main/java/com/example/EmployeeManagement/Controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.example.EmployeeManagement.Controller;
+
+import com.example.EmployeeManagement.Model.Employee;
+import com.example.EmployeeManagement.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee/list/")
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employee list
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployee(){
+        return employeeRepository.findAll();
+    }
+
+}

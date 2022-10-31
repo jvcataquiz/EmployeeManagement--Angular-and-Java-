f6454e4667d1bfe93533adc2261ac7daa48ad54d
jvcataquiz <jvcataquiz@gmail.com>
2022-10-31 12:58:17 +0800

    Created a table where can i display all the data of the employee list from the database(Rest APi) and to test it if it is working i created a object in the employee-listcomponent.ts

--- employee-frontend/src/app/employee-list/employee-list.component.ts
+++ employee-frontend/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
+  employees: Employee[] | undefined;
   constructor() { }
 
   ngOnInit(): void {
+    this.employees =[{
+      "id": 1,
+      "firstName": "Joshua",
+      "lastName": "Dela Crus",
+      "emailAddress": "joshuadelacrus@gmail.com",
+      "address": "Manila City"
+
+    }];
   }
 
 }

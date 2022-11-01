4c83abaee2e47c47d460ec4e1e0f44266445e403
jvcataquiz <jvcataquiz@gmail.com>
2022-11-01 14:32:31 +0800

    added navigation route to create an emplyee

--- EmployeeManagement/src/main/java/com/example/EmployeeManagement/Controller/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/example/EmployeeManagement/Controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.EmployeeManagement.Controller;
 import com.example.EmployeeManagement.Model.Employee;
 import com.example.EmployeeManagement.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
@@ -21,5 +18,10 @@ public class EmployeeController {
     public List<Employee> getAllEmployee(){
         return employeeRepository.findAll();
     }
+    //insert/create employee api
 
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee){
+    return employeeRepository.save(employee);
+    }
 }

4d9ffc9dd3a28786b3ba8197f98ec7052a9246c1
jvcataquiz <jvcataquiz@gmail.com>
2022-11-02 13:18:41 +0800

    Successfully updated a specific record using POst man api

--- EmployeeManagement/src/main/java/com/example/EmployeeManagement/Controller/EmployeeController.java
+++ EmployeeManagement/src/main/java/com/example/EmployeeManagement/Controller/EmployeeController.java
@@ -31,4 +31,18 @@ public class EmployeeController {
         Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee Not Exist!!!"));
         return ResponseEntity.ok(employee);
     }
+
+    //update the employee
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id,@RequestBody Employee employeeDetails){
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee Not Exist!!!"));
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailAddress(employeeDetails.getEmailAddress());
+        employee.setAddress(employeeDetails.getAddress());
+
+       Employee updatedEmployee = employeeRepository.save(employee);
+
+       return ResponseEntity.ok(updatedEmployee);
+    }
 }
